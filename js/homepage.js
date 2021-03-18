3d4a6fe8ab4311f4b5f97ba00cdfa93bfdf53e17
Mona Lisa <joannagrosshans2@gmail.com>
2021-03-18 10:05:02 -0500

    html and js  set up

---
+++ js/homepage.js
@@ -0,0 +1,21 @@
+var getAllRecipes = function(recipe) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+        //request was successful
+        if (response.ok) {
+            response.json().then(function(data) { 
+            displayRepos(data, user);
+        });
+        } else {
+        alert("Error: " + response.statusText); 
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()`
+        alert("Unable to connect to GitHub");
+    });
+};
